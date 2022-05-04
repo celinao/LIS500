4ce18c4ae8cea10ab76e99e13d44b1b574c9679c
rosie-123 <rcao34@wisc.edu>
2022-05-04 01:54:58 -0500

    init contact page

--- contact_us.php
+++ contact_us.php
@@ -9,8 +9,33 @@
 	</head>
 	<body>
 		<?php include 'navbar.php'; ?>
-		
-	
+
+        <div class="container col-xxl-8 px-4 py-5">
+            <h1>Contact Us</h1>
+            <p>
+                How can we help you?
+            </p>
+            <form action="contact_msg.php" method="POST" enctype="multipart/form-data">
+                <div class="mb-3">
+                    <label for="firstname" class="form-label">First Name</label>
+                    <input name="firstname" id="firstname" type="text" class="form-control">
+                </div>
+                <div class="mb-3">
+                    <label for="lastname" class="form-label">Last Name</label>
+                    <input name="lastname" id="lastname" type="text" class="form-control">
+                </div>
+                <div class="mb-3">
+                    <label for="email" class="form-label">Email address</label>
+                    <input name="email" type="email" class="form-control" id="email" required>
+                </div>
+                <div class="mb-3">
+                    <label for="firstname" class="form-label">Message</label>
+                    <textarea name="description" id="description" class="form-control"></textarea>
+                </div>
+                <input name="submit" type="submit" class="btn btn-primary" value="Send My Message">
+            </form>
+        </div>
+
 		<!-- Bootstrap JS-->
 		<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 	</body>
