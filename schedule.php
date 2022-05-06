3b1f4fe5157b65e510d4e9adf848f0abfd240743
Celina Ough <ough@wisc.edu>
2022-05-05 19:25:08 -0500

    Created Schedule

--- schedule.php
+++ schedule.php
@@ -10,6 +10,73 @@
 	<body>
 		<?php include 'navbar.php'; ?>
 		
+		<div class="container col-xxl-8 px-4 py-5">
+		    <h1>Conference Schedule</h1>
+			<ul class="list-group">
+				<h2>Friday</h2>
+				<li	class="list-group-item">
+					<h4 class="text-success">10:00-10:45 | Transitioning within Tech</h4> 
+					Are you considering entering the Tech Industry or transition fields within Tech? This panel has speakers who have all transitioned fields into or within Tech. 
+					Come learn about the steps they took to transition,  the difficulties their faced, and what you can learn from their experiences. 
+				</li>
+				<li	class="list-group-item">
+					<h4 class="text-success">11:00-11:45 | Self-Care in the Age of Work From Home</h4> 
+					As more and more companies are transitioning to a remote or hybrid workday we all must develop new methods of seperating our time between work and home. 
+					Come learn about different methods and techniques you can use to value yourself and not be 'on-call 24/7'. 
+				</li>
+				<li	class="list-group-item list-group-item-light">
+					<h4 class="text-success">11:45-1:00 | Lunch</h4> 
+				</li>
+				<li	class="list-group-item">
+					<h4 class="text-success">1:00-1:45 | Setting yourself up for Financial Success</h4> 
+					How do you set yourself up for financial success in the future? What does it mean to have a 401k or IRA? What's the difference between stocks, ETFs, and mutual funds? 
+					Answer these questions and emerge from this workship with concrete steps to take to set yourself up on the path to finacial security. 
+				</li>
+				<li	class="list-group-item">
+					<h4 class="text-success">2:00-2:45 | Tech Ethics</h4> 
+					Listen to speakers discuss what it means to work in the era of information and what considerations should be made to respect personal privacy. 
+				</li>
+				<li	class="list-group-item">
+					<h4 class="text-success">3:00-3:45 | Code as Art</h4> 
+					More and more people have started CSS to create elaborate artworks. Learn the basics of creating art solely from CSS code and how this movement is inspiring young women 
+					to learn coding. 
+				</li>
+			</ul>
+
+			<br/> 
+
+			<ul class="list-group">
+				<h2>Saturday</h2>
+				<li	class="list-group-item">
+					<h4 class="text-success">10:00-10:45 | Overlooked Talent: Women of Color in Tech</h4> 
+					This session will address intersectionality in Tech as it applies to women of color and the distinct disadvantages they experiences as their identities overlap. 
+					We will discuss why they are underrepresented adn undervalued and what identify ways to rise above and build a community of support that invests in their potential. 
+				</li>
+				<li	class="list-group-item">
+					<h4 class="text-success">11:00-11:45 | Coding for Accessibility</h4> 
+					Accessibility should be considered at every stage in the design and production process when you are creating and improving products. This session will discuss 
+					best practices to use when creating products with a mindset for universal accessibility. 
+				</li>
+				<li	class="list-group-item list-group-item-light">
+					<h4 class="text-success">11:45-1:00 | Lunch</h4> 
+				</li>
+				<li	class="list-group-item">
+					<h4 class="text-success">1:00-1:45 | Communicating Data</h4> 
+					Data is constantly being collected and analyzed but, how do we communicate that data to our audience? There are methods and frameworks you can use when visualizing 
+					and communicating your data and analysis to your audience to succinctly convey your meaning. This talk will go over how to extract the most important factors of your data 
+					and how to create visualizations based on multiple dimensions of data. 
+				</li>
+				<li	class="list-group-item">
+					<h4 class="text-success">2:00-2:45 | Training Data's Invisible Faults</h4> 
+					Training data is a necessary element of all AI and ML models but, what differentiates good and bad training data? How can you even tell what bad data is if you don't know 
+					what you are looking for? This session discusses common faults in different training sets and steps you can take to find and address faults in your own datasets. 
+				</li>
+				<li	class="list-group-item">
+					<h4 class="text-success">3:00-3:45 | Privilege and Power</h4> 
+					Understanding different levels of privilege and power is essential when working to eliminate discrimintation and bias. Use this workshop to take steps to understand 
+					your privilege and what you can do to use your power to change the status quo. 
+				</li>
+			</ul>
 	
 		<!-- Bootstrap JS-->
 		<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
